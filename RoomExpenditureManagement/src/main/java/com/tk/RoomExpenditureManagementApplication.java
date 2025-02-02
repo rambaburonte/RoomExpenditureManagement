e49c07ec3b2df2e8fee8900ae05cd232b11b2117
rambaburonte <147910929+rambaburonte@users.noreply.github.com>
2025-02-03 01:32:04 +0530

    Project creation by Ram

---
+++ RoomExpenditureManagement/src/main/java/com/tk/RoomExpenditureManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomExpenditureManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomExpenditureManagementApplication.class, args);
+	}
+
+}
