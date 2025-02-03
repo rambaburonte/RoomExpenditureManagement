0275d35e5c8473c3f2bc720617f573fccf1b0463
rambaburonte <147910929+rambaburonte@users.noreply.github.com>
2025-02-03 17:24:55 +0530

    home controller method

--- RoomExpenditureManagement/src/main/java/com/tk/controller/RoomController.java
+++ RoomExpenditureManagement/src/main/java/com/tk/controller/RoomController.java
@@ -1,8 +1,13 @@
 package com.tk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RoomController {
-
+	@GetMapping
+	public String home() {
+		return "welcome";
+	}
+	
 }
